193822841f8ee187e590bd836f4275d221689b95
Ariel Lindemann <ariel.lindemann@gmail.com>
2023-12-04 05:04:40 +0100

    day 2 part 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,10 +1,13 @@
 import sys
+import operator
+from functools import reduce
+
 
 def _bag_contents(red: int, green: int, blue: int) -> dict:
     contents = {
-            "red": red,
-            "green": green,
-            "blue": blue
+        "red": red,
+        "green": green,
+        "blue": blue
     }
     return contents
 
@@ -31,6 +34,11 @@ def _parse_contents(s: str) -> dict:
     return contents
 
 
+def _power(s: dict) -> int:
+    amounts = s.values()
+    return reduce(operator.mul, amounts, 1)
+
+
 class Game:
     def __init__(self, game_string: str):
         game_id_str, contents_str = game_string.split(':')
@@ -45,14 +53,25 @@ class Game:
 
         return True
 
+    def min_game(self) -> dict:
+        c = _bag_contents(0, 0, 0)
 
-        
+        for draw in self.draws:
+            for color, amount in draw.items():
+                if amount > c[color]:
+                    c[color] = amount
+
+        return c
+
+
 if __name__ == '__main__':
     part = sys.argv[1]
     input_file = sys.argv[2]
 
     with open(input_file, 'rt') as f:
         input = f.read()
+        lines = input.splitlines()
+        games = [Game(line) for line in lines]
 
         if part == '-1':
             comparison_bag = {
@@ -61,10 +80,11 @@ if __name__ == '__main__':
                 'blue': 14
             }
 
-            lines = input.splitlines()
-            games = [Game(l) for l in lines]
             valid_ids = [g.id for g in games if g.is_valid(comparison_bag)]
 
             print(sum(valid_ids))
 
+        if part == '-2':
+            powers = [_power(g.min_game()) for g in games]
 
+            print(sum(powers))
